f8539a93d37b8a23e5c37e25b386085738305dac
Kurt Lopez <kurtdevph@gmail.com>
2020-10-06 15:30:20 +0800

    Updates

---
+++ precache-manifest.f57aab470bf4115512722b998543b664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf916d6afd1180e044e2f3d4a4778fe0",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "f87eb1b095387afdc8cd",
+    "url": "/robots/static/js/2.81dcf311.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robots/static/js/2.81dcf311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5f362ca5280b2eb182",
+    "url": "/robots/static/js/main.fc106eab.chunk.js"
+  },
+  {
+    "revision": "688f4e7543becb264d41",
+    "url": "/robots/static/js/runtime-main.8a8a4f40.js"
+  },
+  {
+    "revision": "f59c0ffc72718f4301aab4161d1d08d2",
+    "url": "/robots/static/media/plus.f59c0ffc.svg"
+  }
+]);
